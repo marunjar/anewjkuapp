5c2e7c64db1507bd223ef3628ee3ddc413a14ff4
marunjar@aon.at <marunjar@aon.at>
2014-10-28 14:20:49 +0100

    material style sliding tabs
    use material style for tabs like in new play store:
    1. no separators
    2. thinner selector

--- aNewJKUApp/src/main/java/org/voidsink/anewjkuapp/view/SlidingTabStrip.java
+++ aNewJKUApp/src/main/java/org/voidsink/anewjkuapp/view/SlidingTabStrip.java
@@ -29,10 +29,10 @@ class SlidingTabStrip extends LinearLayout {
 
     private static final int DEFAULT_BOTTOM_BORDER_THICKNESS_DIPS = 2;
     private static final byte DEFAULT_BOTTOM_BORDER_COLOR_ALPHA = 0x26;
-    private static final int SELECTED_INDICATOR_THICKNESS_DIPS = 8;
+    private static final int SELECTED_INDICATOR_THICKNESS_DIPS = 4;
 
     private static final int DEFAULT_DIVIDER_THICKNESS_DIPS = 1;
-    private static final byte DEFAULT_DIVIDER_COLOR_ALPHA = 0x20;
+    private static final byte DEFAULT_DIVIDER_COLOR_ALPHA = 0;//0x20;
     private static final float DEFAULT_DIVIDER_HEIGHT = 0.5f;
 
     private final int mBottomBorderThickness;
