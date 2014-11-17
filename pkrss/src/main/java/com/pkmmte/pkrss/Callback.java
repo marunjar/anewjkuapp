36c172b677bfa037e7eac1ce64d432d087f1e584
marunjar@aon.at <marunjar@aon.at>
2014-11-17 08:40:25 +0100

    Revert "add pkrss as module"
    This reverts commit 20ba8e917f49aad3bed903d652e71fb0dc6724a7.

--- pkrss/src/main/java/com/pkmmte/pkrss/Callback.java
+++
@@ -1,9 +0,0 @@
-package com.pkmmte.pkrss;
-
-import java.util.List;
-
-public interface Callback {
-	public void OnPreLoad();
-	public void OnLoaded(List<Article> newArticles);
-	public void OnLoadFailed();
-}
