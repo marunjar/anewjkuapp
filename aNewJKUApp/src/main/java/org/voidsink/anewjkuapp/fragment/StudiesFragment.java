ed1afad220fc4bd84e5ea614a8cf7018fd740d9c
marunjar <marunjar@aon.at>
2015-03-01 22:31:03 +0100

    use recycler view for studies

--- aNewJKUApp/src/main/java/org/voidsink/anewjkuapp/fragment/StudiesFragment.java
+++ aNewJKUApp/src/main/java/org/voidsink/anewjkuapp/fragment/StudiesFragment.java
@@ -5,7 +5,8 @@ import android.content.Intent;
 import android.content.UriMatcher;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -15,27 +16,26 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.timehop.stickyheadersrecyclerview.StickyRecyclerHeadersAdapter;
+import com.timehop.stickyheadersrecyclerview.StickyRecyclerHeadersDecoration;
+
 import org.voidsink.anewjkuapp.KusssContentContract;
 import org.voidsink.anewjkuapp.R;
 import org.voidsink.anewjkuapp.base.BaseContentObserver;
 import org.voidsink.anewjkuapp.base.BaseFragment;
 import org.voidsink.anewjkuapp.base.ContentObserverListener;
-import org.voidsink.anewjkuapp.base.ListWithHeaderAdapter;
+import org.voidsink.anewjkuapp.base.RecyclerArrayAdapter;
 import org.voidsink.anewjkuapp.kusss.Studies;
 import org.voidsink.anewjkuapp.provider.KusssContentProvider;
 import org.voidsink.anewjkuapp.update.UpdateService;
 import org.voidsink.anewjkuapp.utils.Consts;
-import org.voidsink.anewjkuapp.view.ListViewWithHeader;
 
 import java.util.List;
 
-/**
- * Created by paul on 24.11.2014.
- */
 public class StudiesFragment extends BaseFragment implements
         ContentObserverListener {
 
-    private ListWithHeaderAdapter mAdapter;
+    private StudiesAdapter mAdapter;
     private BaseContentObserver mStudiesObserver;
 
     @Override
@@ -61,18 +61,20 @@ public class StudiesFragment extends BaseFragment implements
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_list_with_header, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_recycler_view, container, false);
 
-        final ListViewWithHeader mListView = (ListViewWithHeader) view.findViewById(R.id.list_with_header);
+        final RecyclerView mRecyclerView = (RecyclerView) view.findViewById(R.id.recyclerView);
         mAdapter = new StudiesAdapter(getContext());
-        mListView.setAdapter(mAdapter);
+        mRecyclerView.setAdapter(mAdapter);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        mRecyclerView.addItemDecoration(new StickyRecyclerHeadersDecoration(mAdapter));
 
         return view;
     }
 
     @Override
-    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+    public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
         loadData();
@@ -122,9 +124,7 @@ public class StudiesFragment extends BaseFragment implements
             protected void onPostExecute(Void aVoid) {
 
                 mAdapter.clear();
-                if (mStudies != null) {
-                    mAdapter.addAll(mStudies);
-                }
+                mAdapter.addAll(mStudies);
                 mAdapter.notifyDataSetChanged();
 
                 super.onPostExecute(aVoid);
@@ -138,28 +138,36 @@ public class StudiesFragment extends BaseFragment implements
         loadData();
     }
 
-    private class StudiesAdapter extends ListWithHeaderAdapter<Studies> {
+    private static class StudiesAdapter extends RecyclerArrayAdapter<Studies, StudiesFragment.StudiesViewHolder> implements StickyRecyclerHeadersAdapter<StudiesFragment.StudiesHeaderHolder> {
 
-        private final LayoutInflater mInflater;
+        private final Context mContext;
 
         public StudiesAdapter(Context context) {
-            super(context, R.layout.studies_list_item);
-
-            mInflater = LayoutInflater.from(context);
+            super();
+            mContext = context;
         }
 
         @Override
-        public View getHeaderView(int i, View view, ViewGroup viewGroup) {
-            Studies studies = getItem(i);
-            if (studies != null && !TextUtils.isEmpty(studies.getUni())) {
-                final View mHeaderView = mInflater.inflate(R.layout.list_header, null);
-                final TextView mTitle = (TextView) mHeaderView.findViewById(R.id.list_header_text);
+        public StudiesViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.studies_list_item, parent, false);
+            return new StudiesViewHolder(v);
+        }
 
-                mTitle.setText(studies.getUni());
+        @Override
+        public void onBindViewHolder(StudiesViewHolder holder, int position) {
+            Studies item = getItem(position);
 
-                return mHeaderView;
+            holder.isStandard.setText(item.isStandard() ? mContext.getString(R.string.studies_is_standard_yes) : mContext.getString(R.string.studies_is_standard_no));
+            holder.skz.setText(item.getSkz());
+            holder.title.setText(item.getTitle());
+            holder.steopDone.setText(item.isSteopDone() ? mContext.getString(R.string.studies_steop_done_yes) : mContext.getString(R.string.studies_steop_done_no));
+            holder.activeStatus.setText(item.isActive() ? mContext.getString(R.string.studies_active_status_yes) : mContext.getString(R.string.studies_active_status_no));
+            if (item.getDtStart() != null) {
+                holder.dtStart.setText(item.dateFormat.format(item.getDtStart()));
             }
-            return null;
+            if (item.getDtEnd() != null) {
+                holder.dtEnd.setText(item.dateFormat.format(item.getDtEnd()));
+            }
         }
 
         @Override
@@ -172,74 +180,46 @@ public class StudiesFragment extends BaseFragment implements
         }
 
         @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-            View view = null;
-            Studies item = this.getItem(position);
-            if (item != null) {
-                view = getStudiesView(convertView, parent, item);
-            }
-            return view;
-        }
-
-        private View getStudiesView(View convertView, ViewGroup parent, Studies item) {
-            StudiesHolder holder = null;
-
-            if (convertView == null) {
-                convertView = mInflater.inflate(R.layout.studies_list_item, parent, false);
-                holder = new StudiesHolder();
-
-                holder.isStandard = (TextView) convertView.findViewById(R.id.studies_is_standard);
-                holder.skz = (TextView) convertView.findViewById(R.id.studies_skz);
-                holder.title = (TextView) convertView.findViewById(R.id.studies_title);
-                holder.steopDone = (TextView) convertView.findViewById(R.id.studies_steop_done);
-                holder.activeStatus = (TextView) convertView.findViewById(R.id.studies_active_status);
-                holder.dtStart = (TextView) convertView.findViewById(R.id.studies_dt_start);
-                holder.dtEnd = (TextView) convertView.findViewById(R.id.studies_dt_end);
-
-                convertView.setTag(holder);
-            }
-            if (holder == null) {
-                holder = (StudiesHolder) convertView.getTag();
-            }
-
-            holder.isStandard.setText(item.isStandard() ? getString(R.string.studies_is_standard_yes) : getString(R.string.studies_is_standard_no));
-            holder.skz.setText(item.getSkz());
-            holder.title.setText(item.getTitle());
-            holder.steopDone.setText(item.isSteopDone() ? getString(R.string.studies_steop_done_yes) : getString(R.string.studies_steop_done_no));
-            holder.activeStatus.setText(item.isActive() ? getString(R.string.studies_active_status_yes) : getString(R.string.studies_active_status_no));
-            if (item.getDtStart() != null) {
-                holder.dtStart.setText(item.dateFormat.format(item.getDtStart()));
-            }
-            if (item.getDtEnd() != null) {
-                holder.dtEnd.setText(item.dateFormat.format(item.getDtEnd()));
-            }
-
-            return convertView;
+        public StudiesHeaderHolder onCreateHeaderViewHolder(ViewGroup viewGroup) {
+            View v = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.list_header, viewGroup, false);
+            return new StudiesHeaderHolder(v);
         }
 
         @Override
-        public int getViewTypeCount() {
-            return 1;
+        public void onBindHeaderViewHolder(StudiesHeaderHolder studiesHeaderHolder, int position) {
+            Studies studies = getItem(position);
+            studiesHeaderHolder.mText.setText(studies.getUni());
         }
+    }
 
-        @Override
-        public int getItemViewType(int position) {
-            return 0;
+    public static class StudiesViewHolder extends RecyclerView.ViewHolder {
+        public TextView isStandard;
+        public TextView skz;
+        public TextView title;
+        public TextView steopDone;
+        public TextView activeStatus;
+        public TextView dtStart;
+        public TextView dtEnd;
+
+        public StudiesViewHolder(View itemView) {
+            super(itemView);
+
+            isStandard = (TextView) itemView.findViewById(R.id.studies_is_standard);
+            skz = (TextView) itemView.findViewById(R.id.studies_skz);
+            title = (TextView) itemView.findViewById(R.id.studies_title);
+            steopDone = (TextView) itemView.findViewById(R.id.studies_steop_done);
+            activeStatus = (TextView) itemView.findViewById(R.id.studies_active_status);
+            dtStart = (TextView) itemView.findViewById(R.id.studies_dt_start);
+            dtEnd = (TextView) itemView.findViewById(R.id.studies_dt_end);
         }
+    }
 
-        @Override
-        public boolean isEnabled(int position) {
-            return false;
-        }
+    public static class StudiesHeaderHolder extends RecyclerView.ViewHolder {
+        public TextView mText;
 
-        private class StudiesHolder {
-            public TextView isStandard;
-            public TextView skz;
-            public TextView title;
-            public TextView steopDone;
-            public TextView activeStatus;
-            public TextView dtStart;
-            public TextView dtEnd;
+        public StudiesHeaderHolder(View itemView) {
+            super(itemView);
+            mText = (TextView) itemView.findViewById(R.id.list_header_text);
         }
     }
 }
