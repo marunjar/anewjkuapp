e6627af6912619232699a29c48a4107219e2128f
marunjar <marunjar@aon.at>
2015-03-15 18:04:11 +0100

    follow material guidelines for nav drawer

--- aNewJKUApp/src/main/java/org/voidsink/anewjkuapp/DrawerAdapter.java
+++ aNewJKUApp/src/main/java/org/voidsink/anewjkuapp/DrawerAdapter.java
@@ -1,114 +1,131 @@
 package org.voidsink.anewjkuapp;
 
-import org.voidsink.anewjkuapp.base.BaseArrayAdapter;
-
 import android.content.Context;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import org.voidsink.anewjkuapp.base.BaseArrayAdapter;
+
 public class DrawerAdapter extends BaseArrayAdapter<DrawerItem> {
 
-	private LayoutInflater inflater;
+    private LayoutInflater inflater;
 
-	public DrawerAdapter(Context context) {
-		super(context, R.layout.drawer_menu_item);
+    public DrawerAdapter(Context context) {
+        super(context, R.layout.drawer_menu_item);
 
-		this.inflater = LayoutInflater.from(context);
-	}
-	
-	public DrawerAdapter(Context context, DrawerItem[] objects) {
-		super(context, R.layout.drawer_menu_item, objects);
+        this.inflater = LayoutInflater.from(context);
+    }
 
-		this.inflater = LayoutInflater.from(context);
-	}
+    public DrawerAdapter(Context context, DrawerItem[] objects) {
+        super(context, R.layout.drawer_menu_item, objects);
 
-	@Override
-	public View getView(int position, View convertView, ViewGroup parent) {
-		View view = null;
-		DrawerItem menuItem = this.getItem(position);
-		if (menuItem.isSectionHeader()) {
-			view = getSectionView(convertView, parent, menuItem);
-		} else {
-			view = getItemView(convertView, parent, menuItem);
-		}
-		return view;
-	}
+        this.inflater = LayoutInflater.from(context);
+    }
 
-	private View getItemView(View convertView, ViewGroup parent,
-			DrawerItem drawerMenuItem) {
-		DrawerMenuItem menuItem = (DrawerMenuItem) drawerMenuItem;
-		DrawerMenuItemHolder menuItemHolder = null;
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View view = null;
+        DrawerItem menuItem = this.getItem(position);
+        if (menuItem.isSectionHeader()) {
+            view = getSectionView(convertView, parent, menuItem, position == 0);
+        } else {
+            view = getItemView(convertView, parent, menuItem);
+        }
+        return view;
+    }
 
-		if (convertView == null) {
-			convertView = inflater.inflate(R.layout.drawer_menu_item, parent,
-					false);
-			menuItemHolder = new DrawerMenuItemHolder();
-			menuItemHolder.labelView = (TextView) convertView
-					.findViewById(R.id.drawer_menu_item_label);
-			menuItemHolder.iconView = (ImageView) convertView
-					.findViewById(R.id.drawer_menu_item_icon);
+    private View getItemView(View convertView, ViewGroup parent,
+                             DrawerItem drawerMenuItem) {
+        DrawerMenuItem menuItem = (DrawerMenuItem) drawerMenuItem;
+        DrawerMenuItemHolder menuItemHolder = null;
 
-			convertView.setTag(menuItemHolder);
-		}
+        if (convertView == null) {
+            convertView = inflater.inflate(R.layout.drawer_menu_item, parent,
+                    false);
+            menuItemHolder = new DrawerMenuItemHolder();
+            menuItemHolder.labelView = (TextView) convertView
+                    .findViewById(R.id.drawer_menu_item_label);
+            menuItemHolder.iconView = (ImageView) convertView
+                    .findViewById(R.id.drawer_menu_item_icon);
 
-		if (menuItemHolder == null) {
-			menuItemHolder = (DrawerMenuItemHolder) convertView.getTag();
-		}
+            convertView.setTag(menuItemHolder);
+        }
 
-		menuItemHolder.labelView.setText(menuItem.getLabel(getContext()));
-		menuItemHolder.iconView.setImageResource(menuItem.getIconResID());
+        if (menuItemHolder == null) {
+            menuItemHolder = (DrawerMenuItemHolder) convertView.getTag();
+        }
 
-		return convertView;
-	}
+        menuItemHolder.labelView.setText(menuItem.getLabel(getContext()));
+        menuItemHolder.iconView.setImageResource(menuItem.getIconResID());
 
-	private View getSectionView(View convertView, ViewGroup parent,
-			DrawerItem menuItem) {
-		DrawerMenuSection menuSection = (DrawerMenuSection) menuItem;
-		DrawerMenuSectionHolder menuSectionHolder = null;
+        return convertView;
+    }
 
-		if (convertView == null) {
-			convertView = inflater.inflate(R.layout.drawer_menu_section,
-					parent, false);
-			menuSectionHolder = new DrawerMenuSectionHolder();
-			menuSectionHolder.labelView = (TextView) convertView
-					.findViewById(R.id.navmenusection_label);
-			convertView.setTag(menuSectionHolder);
-		}
+    private View getSectionView(View convertView, ViewGroup parent,
+                                DrawerItem menuItem, boolean isFirstItem) {
+        DrawerMenuSection menuSection = (DrawerMenuSection) menuItem;
+        DrawerMenuSectionHolder menuSectionHolder = null;
 
-		if (menuSectionHolder == null) {
-			menuSectionHolder = (DrawerMenuSectionHolder) convertView.getTag();
-		}
+        if (convertView == null) {
+            convertView = inflater.inflate(R.layout.drawer_menu_section,
+                    parent, false);
+            menuSectionHolder = new DrawerMenuSectionHolder();
+            menuSectionHolder.labelView = (TextView) convertView
+                    .findViewById(R.id.navmenusection_label);
+            menuSectionHolder.separator = convertView.findViewById(R.id.separator);
+            convertView.setTag(menuSectionHolder);
+        }
 
-		menuSectionHolder.labelView.setText(menuSection.getLabel(getContext()));
+        if (menuSectionHolder == null) {
+            menuSectionHolder = (DrawerMenuSectionHolder) convertView.getTag();
+        }
 
-		return convertView;
-	}
+        String mLabel = menuSection.getLabel(getContext());
 
-	@Override
-	public int getViewTypeCount() {
-		return 2;
-	}
+        if (TextUtils.isEmpty(mLabel)) {
+            menuSectionHolder.labelView.setVisibility(View.GONE);
+            menuSectionHolder.separator.setVisibility(View.VISIBLE);
+        } else {
+            menuSectionHolder.labelView.setVisibility(View.VISIBLE);
+            menuSectionHolder.labelView.setText(mLabel);
 
-	@Override
-	public int getItemViewType(int position) {
-		return this.getItem(position).getType();
-	}
+            if (isFirstItem) {
+                menuSectionHolder.separator.setVisibility(View.GONE);
+            } else {
+                menuSectionHolder.separator.setVisibility(View.VISIBLE);
+            }
+        }
 
-	@Override
-	public boolean isEnabled(int position) {
-		return getItem(position).isEnabled();
-	}
+        return convertView;
+    }
 
-	private static class DrawerMenuItemHolder {
-		private TextView labelView;
-		private ImageView iconView;
-	}
+    @Override
+    public int getViewTypeCount() {
+        return 2;
+    }
 
-	private class DrawerMenuSectionHolder {
-		private TextView labelView;
-	}
+    @Override
+    public int getItemViewType(int position) {
+        return this.getItem(position).getType();
+    }
+
+    @Override
+    public boolean isEnabled(int position) {
+        return getItem(position).isEnabled();
+    }
+
+    private static class DrawerMenuItemHolder {
+        private TextView labelView;
+        private ImageView iconView;
+    }
+
+    private class DrawerMenuSectionHolder {
+        private TextView labelView;
+        public View separator;
+    }
 
 }
