5520d58c5caba071f389f0517712824b79181830
marunjar <marunjar@aon.at>
2015-04-06 09:36:01 +0200

    update osmosis

--- tools/mapGenerator/runosmtomap.bat
+++ tools/mapGenerator/runosmtomap.bat
@@ -1,6 +1,7 @@
 @echo off
 
-call .\osmosis\bin\osmosis.bat --rx .\input\campus.osm --mw file=.\output\campus.map
+call .\osmosis\bin\osmosis.bat --read-xml .\input\campus.osm --mapfile-writer file=.\output\campus.map 
+rem bbox=48.2974,14.2603,48.3494,14.3864
 
 pause
 
