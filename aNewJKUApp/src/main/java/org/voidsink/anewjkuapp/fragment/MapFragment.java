80191755bbdbbe9a24501b029828557b0d352de9
marunjar <marunjar@aon.at>
2016-06-15 19:14:49 +0200

    use new rendertheme
    rendertheme-v5.xml

--- aNewJKUApp/src/main/java/org/voidsink/anewjkuapp/fragment/MapFragment.java
+++ aNewJKUApp/src/main/java/org/voidsink/anewjkuapp/fragment/MapFragment.java
@@ -465,7 +465,7 @@ public class MapFragment extends BaseFragment implements
 
     private XmlRenderTheme getRenderTheme() {
         try {
-            return new AssetsRenderTheme(getContext(), "", "renderthemes/rendertheme-v4.xml");
+            return new AssetsRenderTheme(getContext(), "", "renderthemes/rendertheme-v5.xml");
         } catch (Exception e) {
             Analytics.sendException(getContext(), e, false);
         }
