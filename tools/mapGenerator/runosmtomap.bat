ead1a05f55f81283b374bb944cd8df50343c9b2e
marunjar <marunjar@aon.at>
2016-06-15 19:56:22 +0200

    fix convert batch
    dont convert on download error

--- tools/mapGenerator/runosmtomap.bat
+++ tools/mapGenerator/runosmtomap.bat
@@ -8,11 +8,19 @@ echo download new file...
 
 wget -t10 "overpass-api.de/api/map?bbox=14.2323,48.2835,14.4139,48.3629" -O .\input\campus.osm
 
+if %errorlevel% neq 0 goto :error
+
 echo convert osm file...
 
 call .\osmosis\bin\osmosis.bat --read-xml .\input\campus.osm --mapfile-writer file=.\output\campus.map 
 rem bbox=48.2835,14.2323,48.3629,14.4139
 rem overpass-api.de/api/map?bbox=14.2323,48.2835,14.4139,48.3629
+
+pause
+
+goto :eof	
+
+:error
 
 pause
 
