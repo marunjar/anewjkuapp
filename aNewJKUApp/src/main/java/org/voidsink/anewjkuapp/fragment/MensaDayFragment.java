17becd6c485738b9287f0703746fa4dd72a7bac4
marunjar <marunjar@aon.at>
2018-01-17 18:48:20 +0100

    Revert "update WeekView pt2"
    This reverts commit 2714e0f701c1319382b5f3a27f95fd276a3944c9.

--- aNewJKUApp/src/main/java/org/voidsink/anewjkuapp/fragment/MensaDayFragment.java
+++ aNewJKUApp/src/main/java/org/voidsink/anewjkuapp/fragment/MensaDayFragment.java
@@ -1,37 +1,37 @@
 /*
- *       ____.____  __.____ ___     _____
- *      |    |    |/ _|    |   \   /  _  \ ______ ______
- *      |    |      < |    |   /  /  /_\  \\____ \\____ \
- *  /\__|    |    |  \|    |  /  /    |    \  |_> >  |_> >
- *  \________|____|__ \______/   \____|__  /   __/|   __/
- *                   \/                  \/|__|   |__|
+ *      ____.____  __.____ ___     _____
+ *     |    |    |/ _|    |   \   /  _  \ ______ ______
+ *     |    |      < |    |   /  /  /_\  \\____ \\____ \
+ * /\__|    |    |  \|    |  /  /    |    \  |_> >  |_> >
+ * \________|____|__ \______/   \____|__  /   __/|   __/
+ *                  \/                  \/|__|   |__|
  *
- *  Copyright (c) 2014-2017 Paul "Marunjar" Pretsch
+ * Copyright (c) 2014-2015 Paul "Marunjar" Pretsch
  *
- *  This program is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- *  You should have received a copy of the GNU General Public License
- *  along with this program.  If not, see <http://www.gnu.org/licenses/>
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
  *
  */
 
 package org.voidsink.anewjkuapp.fragment;
 
 import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v4.app.LoaderManager;
-import android.support.v4.content.Loader;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -57,9 +57,10 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-public class MensaDayFragment extends BaseFragment implements LoaderManager.LoaderCallbacks<List<MensaItem>> {
+public class MensaDayFragment extends BaseFragment {
 
     private static final String TAG = MensaDayFragment.class.getSimpleName();
+    private static final List<IMensa> mMensen = new ArrayList<>();
     private Date mDate;
     private MensaMenuAdapter mAdapter;
 
@@ -70,25 +71,12 @@ public class MensaDayFragment extends BaseFragment implements LoaderManager.Load
     }
 
     @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-
-        getLoaderManager().initLoader(0, null, this);
-    }
-
-    private void updateData() {
-        if (this.isVisible() && !getLoaderManager().hasRunningLoaders()) {
-            getLoaderManager().restartLoader(0, null, this);
-        }
-    }
-
-    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_recycler_view, container,
                 false);
 
-        final RecyclerView mRecyclerView = view.findViewById(R.id.recyclerView);
+        final RecyclerView mRecyclerView = (RecyclerView) view.findViewById(R.id.recyclerView);
         mRecyclerView.setLayoutManager(new GridLayoutManager(getContext(), 2));
         mAdapter = new MensaMenuAdapter(getContext(), false);
         mRecyclerView.setAdapter(new SectionedRecyclerViewAdapter(mRecyclerView, mAdapter));
@@ -99,66 +87,76 @@ public class MensaDayFragment extends BaseFragment implements LoaderManager.Load
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        new MenuLoadTask(getContext(), new ClassicMenuLoader(), 0).execute();
+        new MenuLoadTask(getContext(), new ChoiceMenuLoader(), 1).execute();
+        new MenuLoadTask(getContext(), new KHGMenuLoader(), 2).execute();
+        new MenuLoadTask(getContext(), new RaabMenuLoader(), 3).execute();
     }
 
     public void setDate(Date mDate) {
         this.mDate = mDate;
     }
 
-    protected List<MenuLoader> createLoaders() {
-        List<MenuLoader> loaders = new ArrayList<>();
-
-        loaders.add(new ClassicMenuLoader());
-        loaders.add(new ChoiceMenuLoader());
-        loaders.add(new KHGMenuLoader());
-        loaders.add(new RaabMenuLoader());
-
-        return loaders;
-    }
-
-    @Override
-    public Loader<List<MensaItem>> onCreateLoader(int id, Bundle args) {
-        showProgressIndeterminate();
-
-        return new MenuDayLoader(getContext(), mDate, createLoaders());
-    }
-
-    @Override
-    public void onLoadFinished(Loader<List<MensaItem>> loader, List<MensaItem> data) {
-        mAdapter.clear();
-        if (data != null) {
-            mAdapter.addAll(data);
+    private synchronized void setMensa(IMensa mensa, int index) {
+        while (index >= mMensen.size()) {
+            mMensen.add(null);
         }
-        mAdapter.notifyDataSetChanged();
 
-        finishProgress();
+        mMensen.set(index, mensa);
     }
 
-    @Override
-    public void onLoaderReset(Loader<List<MensaItem>> loader) {
-        mAdapter.clear();
-        mAdapter.notifyDataSetChanged();
+    private static class MenuDayLoader extends BaseAsyncTaskLoader<ArrayList<MensaItem>> {
+        private final Date mDay;
 
-        finishProgress();
-    }
-
-    private static class MenuDayLoader extends BaseAsyncTaskLoader<List<MensaItem>> {
-        private final Date mDate;
-        private final List<MenuLoader> mMenuLoaders;
-
-        MenuDayLoader(Context c, Date date, List<MenuLoader> mMenuLoaders) {
+        public MenuDayLoader(Context c, Date day) {
             super(c);
-            this.mDate = date;
-            this.mMenuLoaders = mMenuLoaders;
+            this.mDay = day;
         }
 
         @Override
-        public List<MensaItem> loadInBackground() {
+        public ArrayList<MensaItem> loadInBackground() {
+            return null;
+        }
+    }
+
+
+    private class MenuLoadTask extends AsyncTask<String, Void, Void> {
+        private final Context mContext;
+        private final MenuLoader mLoader;
+        private final int mIndex;
+        private IMensa mMensa;
+
+        public MenuLoadTask(Context context, MenuLoader loader, int index) {
+            super();
+
+            this.mContext = context;
+            this.mLoader = loader;
+            this.mIndex = index;
+        }
+
+        @Override
+        protected Void doInBackground(String... urls) {
+            mMensa = mLoader.getMensa(mContext);
+
+            return null;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+            mMensa = null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            setMensa(mMensa, mIndex);
+
             List<MensaItem> menus = new ArrayList<>();
             int noMenuCount = 0;
 
-            for (MenuLoader menuLoader : mMenuLoaders) {
-                IMensa mensa = menuLoader.getMensa(getContext());
+            for (IMensa mensa : mMensen) {
                 if (mensa != null) {
                     IDay day = mensa.getDay(mDate);
                     if (day != null && !day.isEmpty()) {
@@ -167,7 +165,7 @@ public class MensaDayFragment extends BaseFragment implements LoaderManager.Load
                         }
                     } else {
                         // add no menu card
-                        menus.add(new MensaInfoItem(mensa, day, getContext().getString(R.string.mensa_menu_not_available), null));
+                        menus.add(new MensaInfoItem(mensa, day, mContext.getString(R.string.mensa_menu_not_available), null));
                         noMenuCount++;
                     }
                 }
@@ -176,11 +174,16 @@ public class MensaDayFragment extends BaseFragment implements LoaderManager.Load
             // add default no menu card
             if (menus.size() == 0 || menus.size() == noMenuCount) {
                 menus.clear();
-                menus.add(new MensaInfoItem(null, null, getContext().getString(R.string.mensa_menu_not_available), null));
+                menus.add(new MensaInfoItem(null, null, mContext.getString(R.string.mensa_menu_not_available), null));
             }
 
-            return menus;
+            if (mAdapter != null) {
+                mAdapter.clear();
+                mAdapter.addAll(menus);
+                mAdapter.notifyDataSetChanged();
+            }
+
+            super.onPostExecute(result);
         }
     }
-
 }
