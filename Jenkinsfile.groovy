d1e84fe94ac23f7eb2b8da519cc7b873f9dea8a0
marunjar <marunjar@users.noreply.github.com>
2020-10-21 00:45:56 +0200

    Jenkinsfile (#151)
    * add Jenkinsfile

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,40 @@
+node {
+    label 'android'
+
+    timestamps {
+        ansiColor('xterm') {
+            stage('Checkout') {
+                echo 'Checkout.'
+                checkout scm
+            }
+            stage('Build') {
+                echo 'Building..'
+                withGradle {
+                    sh './gradlew assembleFdroid'
+                }
+
+                echo 'Building..'
+                withGradle {
+                    sh './gradlew assembleGoogle'
+                }
+            }
+            stage('Test') {
+                echo 'Testing...'
+            }
+            stage('Analyze') {
+                echo 'Analyzing....'
+                try {
+                    withGradle {
+                        sh './gradlew lintFdroidDebug'
+                    }
+                } finally {
+                    recordIssues blameDisabled: true, forensicsDisabled: true, skipPublishingChecks: true, sourceDirectory: 'app/src', tools: [androidLintParser(pattern: 'app/build/reports/lint-results-fdroidDebug.xml'), errorProne()]
+                }
+            }
+            stage('Deploy') {
+                echo 'Deploying.....'
+                archiveArtifacts artifacts: '**/*.apk', caseSensitive: false, followSymlinks: false
+            }
+        }
+    }
+}
