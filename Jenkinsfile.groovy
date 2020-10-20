c00043c4aae6980be5f06b2048be0fd23ee0cabd
marunjar <marunjar@users.noreply.github.com>
2020-10-20 21:35:57 +0200

    remove steps

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -4,43 +4,33 @@ node {
     timestamps {
         ansiColor('xterm') {
             stage('Build') {
-                steps {
-                    echo 'Building.'
-                    withGradle {
-                        sh './gradlew assembleFdroid'
-                    }
+                echo 'Building.'
+                withGradle {
+                    sh './gradlew assembleFdroid'
                 }
-                steps {
-                    echo 'Building.'
-                    withGradle {
-                        sh './gradlew assembleGoogle'
-                    }
+                echo 'Building.'
+                withGradle {
+                    sh './gradlew assembleGoogle'
                 }
             }
             stage('Test') {
-                steps {
-                    echo 'Testing..'
-                }
+                echo 'Testing..'
             }
             stage('Analyze') {
-                steps {
-                    echo 'Analyzing...'
-                    withGradle {
-                        try {
-                            sh './gradlew lintFdroidDebug'
-                        } finally {
-                            scanForIssues blameDisabled: true, forensicsDisabled: true, sourceDirectory: './app/src', tool: androidLintParser(pattern: './app/build/reports/lint-results-fdroidDebug.xml')
-                        }
+                echo 'Analyzing...'
+                withGradle {
+                    try {
+                        sh './gradlew lintFdroidDebug'
+                    } finally {
+                        scanForIssues blameDisabled: true, forensicsDisabled: true, sourceDirectory: './app/src', tool: androidLintParser(pattern: './app/build/reports/lint-results-fdroidDebug.xml')
                     }
-                    withGradle {
-                        sh './gradlew assembleFdroid'
-                    }
+                }
+                withGradle {
+                    sh './gradlew assembleFdroid'
                 }
             }
             stage('Deploy') {
-                steps {
-                    echo 'Deploying....'
-                }
+                echo 'Deploying....'
             }
         }
     }
