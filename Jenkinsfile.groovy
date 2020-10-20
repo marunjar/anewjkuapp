bd6096935c49f6f8058c3064e968510c117877f7
marunjar <marunjar@users.noreply.github.com>
2020-10-20 21:53:58 +0200

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -4,33 +4,35 @@ node {
     timestamps {
         ansiColor('xterm') {
             def workspace = pwd()
-            stage('Build') {
-                echo 'Building.'
-                echo "${workspace}"
+            dir(workspace) {
+                stage('Build') {
+                    echo 'Building.'
+                    echo workspace
 
-                withGradle {
-                    sh 'cd ${workspace} && gradlew assembleFdroid'
-                }
-                echo 'Building.'
-                withGradle {
-                    sh 'cd ${workspace} && gradlew assembleGoogle'
-                }
-            }
-            stage('Test') {
-                echo 'Testing..'
-            }
-            stage('Analyze') {
-                echo 'Analyzing...'
-                withGradle {
-                    try {
-                        sh 'cd ${workspace} && gradlew lintFdroidDebug'
-                    } finally {
-                        scanForIssues blameDisabled: true, forensicsDisabled: true, sourceDirectory: 'app/src', tool: androidLintParser(pattern: 'app/build/reports/lint-results-fdroidDebug.xml')
+                    withGradle {
+                        sh 'gradlew assembleFdroid'
+                    }
+                    echo 'Building.'
+                    withGradle {
+                        sh 'gradlew assembleGoogle'
                     }
                 }
-            }
-            stage('Deploy') {
-                echo 'Deploying....'
+                stage('Test') {
+                    echo 'Testing..'
+                }
+                stage('Analyze') {
+                    echo 'Analyzing...'
+                    withGradle {
+                        try {
+                            sh 'gradlew lintFdroidDebug'
+                        } finally {
+                            scanForIssues blameDisabled: true, forensicsDisabled: true, sourceDirectory: 'app/src', tool: androidLintParser(pattern: 'app/build/reports/lint-results-fdroidDebug.xml')
+                        }
+                    }
+                }
+                stage('Deploy') {
+                    echo 'Deploying....'
+                }
             }
         }
     }
