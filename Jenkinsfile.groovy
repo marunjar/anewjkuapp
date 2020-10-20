664a9ae4ee54cc81660ed53571245cdb17cb7d62
marunjar <marunjar@users.noreply.github.com>
2020-10-20 21:17:33 +0200

    Create Jenkinsfile.groovy

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,50 @@
+pipeline {
+    agent android
+
+    timestamps {
+        ansiColor('xterm') {
+            stages {
+                stage('Build') {
+                    steps {
+                        echo 'Building.'
+                        withGradle {
+                            sh './gradlew assembleFdroid'
+                        }
+                    }
+                    steps {
+                        echo 'Building.'
+                        withGradle {
+                            sh './gradlew assembleGoogle'
+                        }
+                    }
+                }
+                stage('Test') {
+                    steps {
+                        echo 'Testing..'
+                    }
+                }
+                stage('Analyze') {
+                    steps {
+                        echo 'Analyzing...'
+                        withGradle {
+                            try {
+                                sh './gradlew lintFdroidDebug'
+                            } finally {
+                                scanForIssues blameDisabled: true, forensicsDisabled: true, sourceDirectory: './app/src', tool: androidLintParser(pattern: './app/build/reports/lint-results-fdroidDebug.xml')
+                            }
+                        }
+                        withGradle {
+                            sh './gradlew assembleFdroid'
+                        }
+                    }
+                }
+                stage('Deploy') {
+                    steps {
+                        echo 'Deploying....'
+                    }
+                }
+            }
+        }
+    }
+
+}
