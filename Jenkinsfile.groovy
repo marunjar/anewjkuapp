4bbc00db7d9a3778f4fea7b530469b97080fc92e
marunjar <marunjar@users.noreply.github.com>
2020-11-02 00:14:30 +0100

    Jenkinsfile (#152)
    * Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -8,6 +8,11 @@ node {
             }
             stage('Setup') {
                 properties([buildDiscarder(logRotator(artifactDaysToKeepStr: '', artifactNumToKeepStr: '1', daysToKeepStr: '', numToKeepStr: '5')), [$class: 'ScannerJobProperty', doNotScan: false], disableConcurrentBuilds()])
+                step([$class: 'GitHubCommitStatusSetter', errorHandlers: [[$class: 'ChangingBuildStatusErrorHandler', result: 'UNSTABLE']], statusResultSource: [$class: 'ConditionalStatusResultSource', results: [[$class: 'AnyBuildResult', message: "Build #${env.BUILD_NUMBER} in progress...", state: 'PENDING']]]])
+                withGradle {
+                    sh './gradlew clean'
+                    sh './gradlew cleanBuildCache'
+                }
             }
             stage('Build') {
                 withGradle {
@@ -31,10 +36,7 @@ node {
                 archiveArtifacts artifacts: '**/*.apk', caseSensitive: false, followSymlinks: false
             }
             stage('Cleanup') {
-                withGradle {
-                    sh './gradlew clean'
-                    sh './gradlew cleanBuildCache'
-                }
+                step([$class: 'GitHubCommitStatusSetter', errorHandlers: [[$class: 'ChangingBuildStatusErrorHandler', result: 'UNSTABLE']], statusResultSource: [$class: 'ConditionalStatusResultSource', results: [[$class: 'AnyBuildResult', message: "Build #${env.BUILD_NUMBER} finished!", state: 'SUCCESS']]]])
             }
         }
     }
