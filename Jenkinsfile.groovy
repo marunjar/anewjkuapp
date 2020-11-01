73685876e6c4e9e06b5ac5c180e1103590a6ca3f
marunjar <marunjar@users.noreply.github.com>
2020-11-01 23:56:06 +0100

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -3,42 +3,37 @@ node {
 
     timestamps {
         ansiColor('xterm') {
-            try {
-                stage('Checkout') {
-                    checkout scm
+            stage('Checkout') {
+                checkout scm
+            }
+            stage('Setup') {
+                properties([buildDiscarder(logRotator(artifactDaysToKeepStr: '', artifactNumToKeepStr: '1', daysToKeepStr: '', numToKeepStr: '5')), [$class: 'ScannerJobProperty', doNotScan: false], disableConcurrentBuilds()])
+                step([$class: 'GitHubCommitStatusSetter', errorHandlers: [[$class: 'ChangingBuildStatusErrorHandler', result: 'UNSTABLE']]])
+            }
+            stage('Build') {
+                withGradle {
+                    sh './gradlew assembleFdroid'
+                    sh './gradlew assembleGoogle'
                 }
-                stage('Setup') {
-                    properties([buildDiscarder(logRotator(artifactDaysToKeepStr: '', artifactNumToKeepStr: '1', daysToKeepStr: '', numToKeepStr: '5')), [$class: 'ScannerJobProperty', doNotScan: false], disableConcurrentBuilds()])
-                    step([$class: 'GitHubCommitStatusSetter', errorHandlers: [[$class: 'ChangingBuildStatusErrorHandler', result: 'UNSTABLE']]])
-                }
-                stage('Build') {
+            }
+            stage('Test') {
+                echo 'Testing...'
+            }
+            stage('Analyze') {
+                try {
                     withGradle {
-                        sh './gradlew assembleFdroid'
-                        sh './gradlew assembleGoogle'
+                        sh './gradlew lintFdroidRelease'
                     }
-                    currentBuild.result = 'SUCCESS'
+                } finally {
+                    recordIssues blameDisabled: true, forensicsDisabled: true, skipPublishingChecks: true, sourceDirectory: 'app/src', tools: [androidLintParser(pattern: 'app/build/reports/lint-results-*.xml'), errorProne()]
                 }
-                stage('Test') {
-                    echo 'Testing...'
-                }
-                stage('Analyze') {
-                    try {
-                        withGradle {
-                            sh './gradlew lintFdroidRelease'
-                        }
-                    } finally {
-                        recordIssues blameDisabled: true, forensicsDisabled: true, skipPublishingChecks: true, sourceDirectory: 'app/src', tools: [androidLintParser(pattern: 'app/build/reports/lint-results-*.xml'), errorProne()]
-                    }
-                }
-                stage('Deploy') {
-                    echo '${currentBuild}'
-                    archiveArtifacts artifacts: '**/*.apk', caseSensitive: false, followSymlinks: false
-                }
-            } catch (ignored) {
-                currentBuild.result = 'FAILURE'
+            }
+            stage('Deploy') {
+                echo '${currentBuild}'
+                archiveArtifacts artifacts: '**/*.apk', caseSensitive: false, followSymlinks: false
             }
             stage('Cleanup') {
-                step([$class: 'GitHubCommitStatusSetter', errorHandlers: [[$class: 'ChangingBuildStatusErrorHandler', result: 'UNSTABLE']]])
+                step([$class: 'GitHubCommitStatusSetter', errorHandlers: [[$class: 'ChangingBuildStatusErrorHandler', result: 'UNSTABLE']], statusResultSource: [$class: 'ConditionalStatusResultSource', results: [[$class: 'AnyBuildResult', message: build.description, state: 'SUCCESS']]]])
                 withGradle {
                     sh './gradlew clean'
                     sh './gradlew cleanBuildCache'
