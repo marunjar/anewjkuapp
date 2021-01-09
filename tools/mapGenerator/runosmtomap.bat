ca2192d7ea89e273d09a3496ce0fff1801d04b86
marunjar <marunjar@users.noreply.github.com>
2021-01-09 13:55:20 +0100

    update mapsforge

--- tools/mapGenerator/runosmtomap.bat
+++ tools/mapGenerator/runosmtomap.bat
@@ -11,36 +11,19 @@ if exist .\input\campus.osm (
 )
 
 echo download new files...
+echo.
 
-wget -t10 "overpass-api.de/api/map?bbox=14.2225,48.2361,14.5176,48.3714" -O .\input\linz.osm
-if %errorlevel% neq 0 goto :error
+call :download_file linz "14.2225,48.2361,14.5176,48.3714" 0
 
-wget -t10 "overpass-api.de/api/map?bbox=14.3104,48.3288,14.3292,48.3400" -O .\input\campus.osm
-if %errorlevel% neq 0 goto :error
+call :download_file campus "14.3104,48.3288,14.3292,48.3400" 1
 
-wget -t10 "overpass-api.de/api/map?bbox=14.2993,48.2998,14.3116,48.3058" -O .\input\medcampus.osm
-if %errorlevel% neq 0 goto :error
+call :download_file medcampus "14.2993,48.2998,14.3116,48.3058" 1
 
-wget -t10 "overpass-api.de/api/map?bbox=14.2977,48.3043,14.3009,48.3071" -O .\input\lifesciencecampus.osm
-if %errorlevel% neq 0 goto :error
+call :download_file lifesciencecampus "14.2977,48.3043,14.3009,48.3071" 1
 
-wget -t10 "overpass-api.de/api/map?bbox=14.5104,48.3656,14.5176,48.3714" -O .\input\softwareparkhagenberg.osm
-if %errorlevel% neq 0 goto :error
+call :download_file softwareparkhagenberg "14.5104,48.3656,14.5176,48.3714" 1
 
-wget -t10 "overpass-api.de/api/map?bbox=14.2724,48.3188,14.2780,48.3215" -O .\input\petrinum.osm
-if %errorlevel% neq 0 goto :error
-
-echo convert osm file...
-
-call .\osmosis\bin\osmosis.bat --read-xml .\input\linz.osm --tf accept-ways highway=* --used-node --mapfile-writer file=.\output\linz.map 
-call .\osmosis\bin\osmosis.bat --read-xml .\input\campus.osm --mapfile-writer file=.\output\campus.map 
-call .\osmosis\bin\osmosis.bat --read-xml .\input\medcampus.osm --mapfile-writer file=.\output\medcampus.map 
-call .\osmosis\bin\osmosis.bat --read-xml .\input\lifesciencecampus.osm --mapfile-writer file=.\output\lifesciencecampus.map 
-call .\osmosis\bin\osmosis.bat --read-xml .\input\softwareparkhagenberg.osm --mapfile-writer file=.\output\softwareparkhagenberg.map 
-call .\osmosis\bin\osmosis.bat --read-xml .\input\petrinum.osm --mapfile-writer file=.\output\petrinum.map 
-
-rem bbox=48.2835,14.2323,48.3629,14.4139
-rem overpass-api.de/api/map?bbox=14.2323,48.2835,14.4139,48.3629
+call :download_file petrinum "14.2724,48.3188,14.2780,48.3215" 1
 
 pause
 
@@ -50,5 +33,24 @@ goto :eof
 
 pause
 
-goto :eof	
+goto :eof
 
+:download_file
+echo downloading %1 with bbox=%2...
+echo.
+
+wget -t5 -w10 --random-wait "overpass-api.de/api/map?bbox=%2" -O .\input\%1.osm
+if %errorlevel% neq 0 goto :error
+
+IF %3 GTR 0 (
+    echo converting %1.osm to %1.map...
+	echo.
+    call .\osmosis\bin\osmosis.bat --read-xml .\input\%1.osm --mapfile-writer file=.\output\%1.map 
+) ELSE (
+	echo converting %1.osm to %1.map without details...
+	echo.
+	call .\osmosis\bin\osmosis.bat --read-xml .\input\%1.osm --tf accept-ways highway=* --used-node --mapfile-writer file=.\output\%1.map 
+)
+
+timeout /t 60 /nobreak
+exit /b
