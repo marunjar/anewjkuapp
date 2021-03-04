3aa48b357315e094ffc518ebc85b60c1098e8853
marunjar <marunjar@users.noreply.github.com>
2021-03-04 17:04:20 +0100

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -4,50 +4,52 @@ node {
     timestamps {
         ansiColor('xterm') {
             try {
-            stage('Checkout') {
-                echo 'Checkout...'
-                checkout scm
-            }
-            stage('Setup') {
-                properties([buildDiscarder(logRotator(artifactDaysToKeepStr: '', artifactNumToKeepStr: '2', daysToKeepStr: '', numToKeepStr: '5')), [$class: 'ScannerJobProperty', doNotScan: false], disableConcurrentBuilds()])
-                step([$class: 'GitHubCommitStatusSetter', errorHandlers: [[$class: 'ChangingBuildStatusErrorHandler', result: 'UNSTABLE']], statusResultSource: [$class: 'ConditionalStatusResultSource', results: [[$class: 'AnyBuildResult', message: "Build #${env.BUILD_NUMBER} in progress...", state: 'PENDING']]]])
-                withGradle {
-                    sh './gradlew clean'
-                    sh './gradlew cleanBuildCache'
+                stage('Checkout') {
+                    echo 'Checkout...'
+                    checkout scm
                 }
-            }
-            stage('Build') {
-                withGradle {
-                    sh './gradlew assembleFdroid'
-                    sh './gradlew assembleGoogle'
-                    sh './gradlew bundleFdroid'
-                    sh './gradlew bundleGoogle'
-                }
-            }
-            stage('Test') {
-                echo 'Testing...'
-            }
-            stage('Analyze') {
-                try {
+                stage('Setup') {
+                    properties([buildDiscarder(logRotator(artifactDaysToKeepStr: '', artifactNumToKeepStr: '2', daysToKeepStr: '', numToKeepStr: '5')), [$class: 'ScannerJobProperty', doNotScan: false], disableConcurrentBuilds()])
+                    step([$class: 'GitHubCommitStatusSetter', errorHandlers: [[$class: 'ChangingBuildStatusErrorHandler', result: 'UNSTABLE']], statusResultSource: [$class: 'ConditionalStatusResultSource', results: [[$class: 'AnyBuildResult', message: "Build #${env.BUILD_NUMBER} in progress...", state: 'PENDING']]]])
                     withGradle {
-                        sh './gradlew lintFdroidRelease'
-                        sh './gradlew lintGoogleRelease'
+                        sh './gradlew clean'
+                        sh './gradlew cleanBuildCache'
                     }
-                } finally {
-                    recordIssues blameDisabled: true, forensicsDisabled: true, skipPublishingChecks: true, sourceDirectory: 'app/src', tools: [androidLintParser(pattern: '**/reports/lint-results-*.xml'), errorProne()]
                 }
-            }
-            stage('Deploy') {
-                archiveArtifacts artifacts: '**/outputs/**/*.apk, **/outputs/**/*.aab', caseSensitive: false, followSymlinks: false
-                influxDbPublisher customPrefix: 'anewjkuapp', customProjectName: '', jenkinsEnvParameterField: '', jenkinsEnvParameterTag: '', selectedTarget: 'jenkins'
-            }
-            stage('Cleanup') {
-            }
+                stage('Build') {
+                    withGradle {
+                        sh './gradlew assembleFdroid'
+                        sh './gradlew assembleGoogle'
+                        sh './gradlew bundleFdroid'
+                        sh './gradlew bundleGoogle'
+                    }
+                }
+                stage('Test') {
+                    echo 'Testing...'
+                }
+                stage('Analyze') {
+                    try {
+                        withGradle {
+                            sh './gradlew lintFdroidRelease'
+                            sh './gradlew lintGoogleRelease'
+                        }
+                    } finally {
+                        recordIssues blameDisabled: true, forensicsDisabled: true, skipPublishingChecks: true, sourceDirectory: 'app/src', tools: [androidLintParser(pattern: '**/reports/lint-results-*.xml'), errorProne()]
+                    }
+                }
+                stage('Deploy') {
+                    archiveArtifacts artifacts: '**/outputs/**/*.apk, **/outputs/**/*.aab', caseSensitive: false, followSymlinks: false
+                    influxDbPublisher customPrefix: 'anewjkuapp', customProjectName: '', jenkinsEnvParameterField: '', jenkinsEnvParameterTag: '', selectedTarget: 'jenkins'
+                }
+                stage('Cleanup') {
+                }
             } finally {
-            stage('Notify') {
-                step([$class: 'GitHubCommitStatusSetter', errorHandlers: [[$class: 'ChangingBuildStatusErrorHandler', result: 'UNSTABLE']], statusResultSource: [$class: 'ConditionalStatusResultSource', results: [[$class: 'AnyBuildResult', message: "Build #${env.BUILD_NUMBER} finished!", state: 'SUCCESS']]]])
-                emailext attachLog: true, body: '', recipientProviders: [buildUser(), requestor()], subject: ''
-            }
+                stage('Notify') {
+                    step([$class: 'GitHubCommitStatusSetter', errorHandlers: [[$class: 'ChangingBuildStatusErrorHandler', result: 'UNSTABLE']], statusResultSource: [$class: 'ConditionalStatusResultSource', results: [[$class: 'AnyBuildResult', message: "Build #${env.BUILD_NUMBER} finished!", state: 'SUCCESS']]]])
+                    emailext attachLog: true, body: '''$PROJECT_NAME - Build # $BUILD_NUMBER - $BUILD_STATUS:
+
+Check console output at $BUILD_URL to view the results.''', recipientProviders: [buildUser(), requestor()], subject: '$PROJECT_NAME - Build # $BUILD_NUMBER - $BUILD_STATUS!'
+                }
             }
         }
     }
