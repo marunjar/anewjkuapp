745e92cc2f0b22b161e13766a87494c664f641c2
marunjar <marunjar@users.noreply.github.com>
2021-07-30 22:53:21 +0200

    Update Jenkinsfile.groovy
    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,20 +1,30 @@
-node {
-    label 'android'
-
-    timestamps {
-        ansiColor('xterm') {
-            stage('Checkout') {
-                echo 'Checkout...'
+pipeline {
+    agent {
+        label 'android'
+    }
+    options {
+        skipDefaultCheckout()
+        timestamps()
+        ansiColor('xterm')
+        buildDiscarder(logRotator(artifactDaysToKeepStr: '', artifactNumToKeepStr: '2', daysToKeepStr: '', numToKeepStr: '5'))
+        disableConcurrentBuilds()
+    }
+    stages {
+        stage('Checkout') {
+            steps {
                 checkout scm
             }
-            stage('Setup') {
-                properties([buildDiscarder(logRotator(artifactDaysToKeepStr: '', artifactNumToKeepStr: '2', daysToKeepStr: '', numToKeepStr: '5')), [$class: 'ScannerJobProperty', doNotScan: false], disableConcurrentBuilds()])
-                step([$class: 'GitHubCommitStatusSetter', errorHandlers: [[$class: 'ChangingBuildStatusErrorHandler', result: 'UNSTABLE']], statusResultSource: [$class: 'ConditionalStatusResultSource', results: [[$class: 'AnyBuildResult', message: "Build #${env.BUILD_NUMBER} in progress...", state: 'PENDING']]]])
+        }
+        stage('Setup') {
+            steps {
+                step([$class: 'GitHubSetCommitStatusBuilder'])
                 withGradle {
                     sh './gradlew clean'
                 }
             }
-            stage('Build') {
+        }
+        stage('Build') {
+            steps {
                 withGradle {
                     sh './gradlew assembleGoogle'
                     sh './gradlew bundleGoogle'
@@ -22,31 +32,44 @@ node {
                     sh './gradlew bundleFdroid'
                 }
             }
-            stage('Test') {
+        }
+        stage('Test') {
+            steps {
                 echo 'Testing...'
             }
-            stage('Analyze') {
-                try {
-                    withGradle {
-                        sh './gradlew lintGoogleRelease'
-                        sh './gradlew lintFdroidRelease'
-                    }
-                } finally {
-                    recordIssues blameDisabled: true, forensicsDisabled: true, skipPublishingChecks: true, sourceDirectory: 'app/src', tools: [androidLintParser(pattern: '**/reports/lint-results-*.xml'), errorProne()]
+        }
+        stage('Analyze') {
+            steps {
+                withGradle {
+                    sh './gradlew lintGoogleRelease'
+                    sh './gradlew lintFdroidRelease'
                 }
             }
-            stage('Deploy') {
+        }
+        stage('Deploy') {
+            when {
+                expression {
+                    currentBuild.result == null || currentBuild.result == 'SUCCESS'
+                }
+            }
+            steps {
                 archiveArtifacts artifacts: '**/outputs/**/*.apk, **/outputs/**/*.aab', caseSensitive: false, followSymlinks: false
-                influxDbPublisher customPrefix: 'anewjkuapp', customProjectName: '', jenkinsEnvParameterField: '', jenkinsEnvParameterTag: '', selectedTarget: 'jenkins'
             }
-            stage('Cleanup') {
-            }
-            stage('Notify') {
-                step([$class: 'GitHubCommitStatusSetter', errorHandlers: [[$class: 'ChangingBuildStatusErrorHandler', result: 'UNSTABLE']], statusResultSource: [$class: 'ConditionalStatusResultSource', results: [[$class: 'AnyBuildResult', message: "Build #${env.BUILD_NUMBER} finished!", state: 'SUCCESS']]]])
-                emailext attachLog: true, body: '''$PROJECT_NAME - Build # $BUILD_NUMBER - $BUILD_STATUS:
+        }
+    }
+    post {
+        success {
+            recordIssues skipBlames: true, skipPublishingChecks: true, sourceDirectory: 'app/src', tools: [androidLintParser(pattern: '**/reports/lint-results-*.xml'), errorProne()]
+            influxDbPublisher customPrefix: 'anewjkuapp', customProjectName: '', jenkinsEnvParameterField: '', jenkinsEnvParameterTag: '', selectedTarget: 'jenkins'
+        }
+        always {
+            step([$class: 'GitHubCommitStatusSetter'])
+            emailext attachLog: true, body: '''$PROJECT_NAME - Build # $BUILD_NUMBER - $BUILD_STATUS:
 
 Check console output at $BUILD_URL to view the results.''', recipientProviders: [buildUser(), requestor()], subject: '$PROJECT_NAME - Build # $BUILD_NUMBER - $BUILD_STATUS!'
-            }
+        }
+        cleanup {
+            echo 'Cleanup...'
         }
     }
 }
